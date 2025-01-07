562c76da7d44aae2ce414b9519b8e6ec1653d92a
vzegers <vicente.zegers@alumnos.usm.cl>
2025-01-07 11:35:00 -0300

    ETL_FLR: Add check process sh

---
+++ check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Nombre del script PHP
+PHP_SCRIPT="/var/www/etl_flr/run.php"
+
+# Comprobar si el script PHP está corriendo
+if ! pgrep -f "php $PHP_SCRIPT" > /dev/null; then
+    echo "El script PHP no está corriendo. Iniciando el script..."
+    # Ejecutar el script PHP en segundo plano
+    nohup php $PHP_SCRIPT > /dev/null 2>&1 &
+else
+    echo "El script PHP ya está corriendo."
+fi
+
